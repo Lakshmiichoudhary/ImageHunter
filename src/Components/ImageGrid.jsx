7df4c84f936fe529140cf99c7d806d1a87e49e79
lakshmiichoudhary <laxmi2252001@gmail.com>
2024-04-12 16:14:28 +0530

    added shimmer effect

--- src/Components/ImageGrid.jsx
+++ src/Components/ImageGrid.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
+import Shimmer from '../Utils/Shimmer'
 
 const ImageGrid = ({images}) => {
+
   return (
     <div>
-      <div className="p-2 lg:flex overflow-hidden flex-wrap mx-32">
-      {images.map((image) => (
-        <div key={image.id} className="p-2 mx-2 shadow-lg rounded-md from-black mt-4">
+      <div className="p-2 lg:flex overflow-hidden flex-wrap lg:mx-28">
+      {images && images.length > 0 ? (
+      images.map((image) => (
+        <div key={image.id} className="p-2 mx-4 shadow-2xl rounded-md from-slate-950 mt-4">
           <img className='p-4 w-80 h-72'
           src={image.previewURL} alt={image.tags} />
         </div>
-      ))}
+      ))
+      ) : (<Shimmer />
+    )}
     </div>
     </div>
   )
---
+++ src/Utils/Shimmer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Shimmer = () => {
+  return (
+    <>
+      <div className="p-2 mx-2 shadow-lg rounded-md from-black mt-4">
+        <div className="animate-pulse bg-slate-700 h-72 w-80"></div>
+      </div>
+      <div className="p-2 mx-2 shadow-lg rounded-md from-black mt-4">
+        <div className="animate-pulse  bg-slate-700 h-72 w-80"></div>
+      </div>
+      <div className="p-2 mx-2 shadow-lg rounded-md from-black mt-4">
+        <div className="animate-pulse  bg-slate-700 h-72 w-80"></div>
+      </div>
+      <div className="p-2 mx-2 shadow-lg rounded-md from-black mt-4">
+        <div className="animate-pulse  bg-slate-700 h-72 w-80"></div>
+      </div>
+      <div className="p-2 mx-2 shadow-lg rounded-md from-black mt-4">
+        <div className="animate-pulse  bg-slate-700 h-72 w-80"></div>
+      </div>
+      <div className="p-2 mx-2 shadow-lg rounded-md from-black mt-4">
+        <div className="animate-pulse  bg-slate-700 h-72 w-80"></div>
+      </div>
+    </>
+  )
+}
+
+export default Shimmer
